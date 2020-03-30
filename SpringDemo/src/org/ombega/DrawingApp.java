efeaba65db87e2239e6f96db4e8755a0dd7233ec
Brian Ombega <brianombega@gmail.com>
2020-03-30 16:00:55 +0300

    Changed DrawingApp to use ApplicationContext instead of BeanFactory

--- SpringDemo/src/org/ombega/DrawingApp.java
+++ SpringDemo/src/org/ombega/DrawingApp.java
@@ -18,6 +18,7 @@ public class DrawingApp {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
+		
   
 	}
 
--- SpringDemo/src/org/ombega/Triangle.java
+++ SpringDemo/src/org/ombega/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
 	
     public String getType() {
 		return type;
-	}
+    }
 
 
 	public void setType(String type) {
