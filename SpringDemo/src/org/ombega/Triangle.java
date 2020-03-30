5d9ac43fa4ab28edef36a2d062dd37763351fcf1
Brian Ombega <brianombega@gmail.com>
2020-03-30 16:38:25 +0300

    changed spring.xml to use a constructor instead of a setter to inject dependency

--- SpringDemo/src/org/ombega/Triangle.java
+++ SpringDemo/src/org/ombega/Triangle.java
@@ -2,19 +2,31 @@ package org.ombega;
 
 public class Triangle {
 	private String type;
+	private int height;
 	
-	
+	//constructors
+	public Triangle(String type) {
+		this.type = type;
+	}
+	public Triangle(int height) {
+		this.height = height;
+	}
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	public int getHeight() {
+		return height;
+	}
     public String getType() {
 		return type;
     }
 
-
-	public void setType(String type) {
-		this.type = type;
-	}
-
+	//public void setType(String type) {
+	//	this.type = type;
+	//}
 
 	public void draw() {
-    	System.out.println(getType() + " triangle has been drawn");
+    	System.out.println(getType() + " triangle drawn of height " + getHeight());
     }
 }
