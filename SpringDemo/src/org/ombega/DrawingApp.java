c5b4aef63f7eec31ef991f33f75a703a60a89e9e
Brian Ombega <brianombega@gmail.com>
2020-03-30 15:59:02 +0300

    Changed DrawingApp.Java to use ApplicationContext instead of BeanFactory

--- SpringDemo/src/org/ombega/DrawingApp.java
+++ SpringDemo/src/org/ombega/DrawingApp.java
@@ -2,6 +2,8 @@ package org.ombega;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
@@ -9,10 +11,12 @@ public class DrawingApp {
 	public static void main(String[] args) {
 		//Triangle triangle = new Triangle();
 		// create a new instance of the BeanFactory object, call it factory
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/spring.xml"));
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("src/spring.xml"));
 		// the factory object has a method called getBean that takes in bean id
-		Triangle triangle = (Triangle) factory.getBean("triangle");
-		
+		//Triangle triangle = (Triangle) factory.getBean("triangle");
+		//Using ApplicationContext instead of BeanFactory which has additional features such as event notification and AOP
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
   
 	}
--- SpringDemo/src/org/ombega/Triangle.java
+++ SpringDemo/src/org/ombega/Triangle.java
@@ -1,7 +1,20 @@
 package org.ombega;
 
 public class Triangle {
-    public void draw() {
-    	System.out.println("Triangle has been drawn");
+	private String type;
+	
+	
+    public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public void draw() {
+    	System.out.println(getType() + " triangle has been drawn");
     }
 }
