546b1518ca819c28dac65d78f28e7e4ecc3d1748
Brian Ombega <brianombega@gmail.com>
2020-03-31 11:01:54 +0300

    Added Point.java

--- SpringDemo/src/org/ombega/Triangle.java
+++ SpringDemo/src/org/ombega/Triangle.java
@@ -1,32 +1,37 @@
 package org.ombega;
 
 public class Triangle {
-	private String type;
-	private int height;
-	
-	//constructors
-	public Triangle(String type) {
-		this.type = type;
-	}
-	public Triangle(int height) {
-		this.height = height;
-	}
-	public Triangle(String type, int height) {
-		this.type = type;
-		this.height = height;
-	}
-	public int getHeight() {
-		return height;
-	}
-    public String getType() {
-		return type;
-    }
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	//public void setType(String type) {
-	//	this.type = type;
-	//}
+	public Point getPointA() {
+		return pointA;
+	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
 
 	public void draw() {
-    	System.out.println(getType() + " triangle drawn of height " + getHeight());
+    	System.out.println("Point A is (" + getPointA().getX() +"," + getPointA().getY() +")");
+    	System.out.println("Point B is (" + getPointB().getX() +"," + getPointB().getY() +")");
+    	System.out.println("Point C is (" + getPointC().getX() +"," + getPointC().getY() +")");
     }
 }
