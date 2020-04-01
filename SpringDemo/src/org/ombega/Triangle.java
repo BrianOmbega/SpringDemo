397248771ec65e9b32eb492e330c9b8e4c54a243
Brian Ombega <brianombega@gmail.com>
2020-04-01 10:51:16 +0300

    Autowired "byName" in spring.xml

--- SpringDemo/src/org/ombega/Triangle.java
+++ SpringDemo/src/org/ombega/Triangle.java
@@ -1,26 +1,41 @@
 package org.ombega;
 
-import java.util.List;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+
+/*implement the ApplicationContextAware interface to allpw Triangle.java use the "context"
+  instantiated in DrawingApp.java*/
+public class Triangle implements ApplicationContextAware, BeanNameAware {
+	
      private Point pointA;
      private Point pointB;
      private Point pointC;
+     private ApplicationContext context = null;
+     
 	public Point getPointA() {
 		return pointA;
 	}
+	
 	public void setPointA(Point pointA) {
 		this.pointA = pointA;
 	}
+	
 	public Point getPointB() {
 		return pointB;
 	}
+	
 	public void setPointB(Point pointB) {
 		this.pointB = pointB;
 	}
+	
 	public Point getPointC() {
 		return pointC;
 	}
+	
 	public void setPointC(Point pointC) {
 		this.pointC = pointC;
 	}
@@ -29,6 +44,17 @@ public class Triangle {
     	System.out.println("Point A is (" + getPointA().getX() +"," + getPointA().getY() +")");
     	System.out.println("Point B is (" + getPointB().getX() +"," + getPointB().getY() +")");
     	System.out.println("Point C is (" + getPointC().getX() +"," + getPointC().getY() +")");
-    } 
+    }
+
+	@Override
+	public void setApplicationContext(ApplicationContext context) throws BeansException {
+		this.context = context;		
+	}
+
+	@Override
+	public void setBeanName(String beanName) {
+		System.out.println("Bean name is:" + beanName);
+		
+	} 
 
 }
